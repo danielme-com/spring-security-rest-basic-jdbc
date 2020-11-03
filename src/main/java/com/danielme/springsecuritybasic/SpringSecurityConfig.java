424d3492f6f8d0dd66aac6b9c86c86d5d31ab179
dani <danielme_com@yahoo.com>
2020-11-03 20:33:33 +0100

    inyecciones en constructor

--- src/main/java/com/danielme/springsecuritybasic/SpringSecurityConfig.java
+++ src/main/java/com/danielme/springsecuritybasic/SpringSecurityConfig.java
@@ -1,34 +1,35 @@
 package com.danielme.springsecuritybasic;
 
-import javax.sql.DataSource;
-
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
-import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.crypto.factory.PasswordEncoderFactories;
-import org.springframework.security.crypto.password.PasswordEncoder;
+
+import javax.sql.DataSource;
 
 @Configuration
 @EnableWebSecurity
 //@EnableGlobalMethodSecurity(prePostEnabled = true)
 public class SpringSecurityConfig extends WebSecurityConfigurerAdapter {
 
-    @Autowired
-    private DataSource dataSource;
+    private final DataSource dataSource;
 
-    @Value("${security-jdbc.user}")
-    private String usersByUsernameQuery;
+    private final String usersByUsernameQuery;
 
-    @Value("${security.jdbc-authorities}")
-    private String authoritiesByUsernameQuery;
+    private final String authoritiesByUsernameQuery;
+
+    public SpringSecurityConfig(DataSource dataSource,
+                                @Value("${security-jdbc.user}") String usersByUsernameQuery,
+                                @Value("${security.jdbc-authorities}") String authoritiesByUsernameQuery) {
+        this.dataSource = dataSource;
+        this.usersByUsernameQuery = usersByUsernameQuery;
+        this.authoritiesByUsernameQuery = authoritiesByUsernameQuery;
+    }
 
     @Override
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
@@ -40,11 +41,11 @@ public class SpringSecurityConfig extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
-        .and().csrf().disable()
-        .authorizeRequests().antMatchers(HttpMethod.GET, "/country/**").authenticated()
-        .antMatchers(HttpMethod.DELETE, "/country/**").hasRole("ADMIN")
-        .antMatchers("/**").permitAll()
-        .and().httpBasic();
+                .and().csrf().disable()
+                .authorizeRequests().antMatchers(HttpMethod.GET, "/country/**").authenticated()
+                .antMatchers(HttpMethod.DELETE, "/country/**").hasRole("ADMIN")
+                .antMatchers("/**").permitAll()
+                .and().httpBasic();
     }
     
     /*@Override
